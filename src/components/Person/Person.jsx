c517520edbd47f4453d2ef6c976dcadc7ced427d
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-04-10 20:05:48 -0300

    adjustments

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,15 @@
 export const Person = ({ person }) => (
-<>
-  <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-  {person.age ? <p className="Person__age">I am 37</p>: ''}
-  {person.isMarried
-    ? <p className="Person__partner">{person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}</p>
-    : <p className="Person__partner">I am not married</p>}
-</>
-
-
+  <>
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {person.age ? <p className="Person__age">I am 37</p> : ''}
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  </>
 );
